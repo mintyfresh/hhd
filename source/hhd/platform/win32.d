91db6cefa6555ece6e5da8061af7784fb4505adc
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2024-05-08 23:57:11 -0400

    Handmade day 003.

--- source/hhd/platform/win32.d
+++ source/hhd/platform/win32.d
@@ -3,77 +3,156 @@ module hhd.platform.win32;
 debug import core.stdc.stdio;
 import core.sys.windows.windows;
 
+// TODO: Extract this from global state
+__gshared bool isRunning;
+__gshared BITMAPINFO bitmapInfo;
+__gshared void* bitmapMemory;
+__gshared HBITMAP bitmapHandle;
+__gshared HDC bitmapDeviceContext;
+
+@nogc
+private void
+win32ResizeDIBSection(int width, int height) nothrow
+{
+    // TODO: Bulletproof this
+    // Maybe don't free first, free after, then free first if that fails
+
+    if (bitmapHandle)
+    {
+        DeleteObject(bitmapHandle);
+    }
+
+    if (bitmapDeviceContext is null)
+    {
+        // TODO: Should we recreate this ever?
+        bitmapDeviceContext = CreateCompatibleDC(null);
+    }
+
+    BITMAPINFOHEADER bitmapHeader = {
+        biSize: BITMAPINFOHEADER.sizeof,
+        biWidth: width,
+        biHeight: height,
+        biPlanes: 1,
+        biBitCount: 32,
+        biCompression: BI_RGB
+    };
+    bitmapInfo.bmiHeader = bitmapHeader;
+
+    debug
+    {
+        assert(bitmapHeader.biXPelsPerMeter == 0, "Expected 0");
+        assert(bitmapHeader.biYPelsPerMeter == 0, "Expected 0");
+    }
+
+    bitmapHandle = CreateDIBSection(
+        bitmapDeviceContext,
+        &bitmapInfo,
+        DIB_RGB_COLORS,
+        &bitmapMemory,
+        null, 0
+    );
+}
+
+@nogc
+private void
+win32UpdateWindow(HDC deviceContext, int x, int y, int width, int height) nothrow
+{
+    StretchDIBits(
+        deviceContext,
+        x, y, width, height,
+        x, y, width, height, // TODO: input buffer dimensions
+        bitmapMemory, &bitmapInfo,
+        DIB_RGB_COLORS, SRCCOPY
+    );
+}
+
+@nogc
 extern (Windows) LRESULT
 win32WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam) nothrow
 {
     LRESULT result;
 
-    switch (message)
+    try
     {
-        // case WM_SIZE:
-        // {
-        //     debug printf("Message: WM_SIZE\n");
-        // }
-        // break;
-
-        // case WM_DESTROY:
-        // {
-        //     debug printf("Message: WM_DESTROY\n");
-        // }
-        // break;
-
-        // case WM_CLOSE:
-        // {
-        //     debug printf("Message: WM_CLOSE\n");
-        // }
-        // break;
-
-        // case WM_ACTIVATEAPP:
-        // {
-        //     debug printf("Message: WM_ACTIVEAPP\n");
-        // }
-        // break;
-
-        case WM_PAINT:
+        switch (message)
         {
-            PAINTSTRUCT paint;
-            HDC deviceContext = BeginPaint(window, &paint);
-
-            static DWORD operation = WHITENESS;
-
-            PatBlt(
-                deviceContext,
-                paint.rcPaint.left,
-                paint.rcPaint.top,
-                paint.rcPaint.right - paint.rcPaint.left,
-                paint.rcPaint.bottom - paint.rcPaint.top,
-                operation
-            );
-
-            if (operation == WHITENESS)
+            case WM_CLOSE:
             {
-                operation = BLACKNESS;
+                // TODO: Prompt the user to confirm
+                isRunning = false;
             }
-            else
-            {
-                operation = WHITENESS;
-            }
+            break;
 
-            EndPaint(window, &paint);
+            case WM_DESTROY:
+            {
+                // TODO: Was this an error?
+                isRunning = false;
+            }
+            break;
+
+            case WM_SIZE:
+            {
+                RECT clientRect;
+                GetClientRect(window, &clientRect);
+
+                int width = clientRect.right - clientRect.left;
+                int height = clientRect.bottom - clientRect.top;
+
+                win32ResizeDIBSection(width, height);
+            }
+            break;
+
+            // case WM_ACTIVATEAPP:
+            // {
+            //     debug printf("Message: WM_ACTIVEAPP\n");
+            // }
+            // break;
+
+            case WM_PAINT:
+            {
+                PAINTSTRUCT paint;
+                HDC deviceContext = BeginPaint(window, &paint);
+
+                PatBlt(
+                    deviceContext,
+                    paint.rcPaint.left,
+                    paint.rcPaint.top,
+                    paint.rcPaint.right - paint.rcPaint.left,
+                    paint.rcPaint.bottom - paint.rcPaint.top,
+                    WHITENESS
+                );
+
+                EndPaint(window, &paint);
+            }
+            break;
+
+            default:
+            {
+                result = DefWindowProc(window, message, wParam, lParam);
+            }
         }
-        break;
-
-        default:
+    }
+    catch (Throwable t) // TODO: Remove this in release (only debug can raise exceptions)
+    {
+        debug
         {
-            result = DefWindowProc(window, message, wParam, lParam);
+            enum MAX_ERROR_LENGTH = 2500;
+            char[MAX_ERROR_LENGTH + 1] buffer;
+            string error = t.toString();
+            size_t length = error.length > MAX_ERROR_LENGTH ? MAX_ERROR_LENGTH : error.length;
+            buffer[0..length] = t.toString()[0..length];
+            buffer[$ - 1] = 0; // Ensure null-termination
+            printf("Error: %s\n", buffer.ptr);
         }
+
+        PostQuitMessage(-1);
     }
 
     return result;
 }
 
 @nogc
-int
+extern (Windows) int
 main() nothrow
 {
     HINSTANCE instance = GetModuleHandle(NULL);
@@ -102,10 +181,8 @@ main() nothrow
         windowClass.lpszClassName,
         "HHD",
         WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT,
         null,
         null,
         instance,
@@ -118,7 +195,9 @@ main() nothrow
         printf("Created window successfully\n");
     }
 
-    while (true)
+    isRunning = true;
+
+    while (isRunning)
     {
         MSG message;
         BOOL result = GetMessage(&message, window, 0, 0);
